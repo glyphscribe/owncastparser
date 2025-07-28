7984ff05a795f95e8346ffb7733c8a2b252153ac
Glyphscribe Ety <ety@mch.onl>
2025-07-28 16:01:59 -0500

    jiggle the handle

--- custom_components/owncastparser/sensor.py
+++ custom_components/owncastparser/sensor.py
@@ -10,7 +10,7 @@ import async_timeout
 import voluptuous as vol
 
 from homeassistant.components.sensor import SensorEntity, PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME, CONF_TIMEOUT, CONF_URL, CONF_VERIFY_SSL, CONF_SCAN_INTERVAL
+from homeassistant.const import CONF_NAME, CONF_SCAN_INTERVAL
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
@@ -19,22 +19,22 @@ if TYPE_CHECKING:
     from homeassistant.helpers.entity_platform import AddEntitiesCallback
     from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
+# YAML Sensor Variables
+CONF_OWNCAST_URL = "owncast_url"
+CONF_TIMEOUT = "timeout"
+CONF_VERIFY_SSL = "verify"
 
-
-COMPOENENT_REPO = "https://github.com/glyphscribe/owncastparser" # copy
-
-CONF_SERVER_URL = "url"
-
-DEFAULT_SCAN_INTERVAL = timedelta(minutes=1)
-DEFAULT_TIMEOUT = 10
+# Default Values
+DEFAULT_TIMEOUT = 10 # Seconds
 DEFAULT_VERIFY_SSL = True
+DEFAULT_SCAN_INTERVAL = timedelta(minutes=1)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_NAME): cv.string,
-        vol.Required(CONF_SERVER_URL): cv.string,
-        vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): vol.All(int, vol.Range(min=1, max=300)),
-        vol.Optional(CONF_VERIFY_SSL, default=DEFAULT_VERIFY_SSL): cv.boolean,
+        vol.Required(CONF_OWNCAST_URL): cv.string, # can this be replaced with a builtin?
+        vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int, # can this be replaced with a builtin?
+        vol.Optional(CONF_VERIFY_SSL, default=DEFAULT_VERIFY_SSL): cv.boolean, # can this be replaced with a builtin?
         vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.time_period
     },
 )
@@ -45,17 +45,16 @@ async def async_setup_platform(
         hass: HomeAssistant,
         config: ConfigType,
         async_add_devices: AddEntitiesCallback,
-        discovery_info: DiscoveryInfoType | None = None
+        discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     async_add_devices(
         [
             OwncastParserSensor(
-                hass=hass,
-                name = config[CONF_NAME],
-                server_url = config[CONF_SERVER_URL],
-                timeout = config[CONF_TIMEOUT],
-                verify_ssl = config[CONF_VERIFY_SSL],
-                scan_interval = config[CONF_SCAN_INTERVAL]
+                url=config[CONF_OWNCAST_URL],
+                name=config[CONF_NAME],
+                timeout=config[CONF_TIMEOUT],
+                verify_ssl=config[CONF_VERIFY_SSL],
+                scan_interval=config[CONF_SCAN_INTERVAL]
             ),
         ],
         update_before_add=True,
@@ -66,62 +65,52 @@ class OwncastParserSensor(SensorEntity):
 
     def __init__(
             self: OwncastParserSensor,
-            hass: HomeAssistant,
+            url: str,
             name: str,
-            server_url: str,
             timeout: int,
             verify_ssl: bool,
+            scan_interval: timedelta
     ) -> None:
-        self._hass = hass
-        self._name = name
-        self._server_url = server_url
+        self._url = url
+        self._attr_name = name
+        self._attr_icon = "mdi:something"
         self._timeout = timeout
         self._verify_ssl = verify_ssl
+        self._scan_interval = scan_interval
+        self._attr_extra_state_attributes = {"viewers": 0}
+        self._attr_attribution = "Data retrieved using Owncast Parser"
+        _LOGGER.debug(f"Owncast Tracker for {self.name} initialized.")
 
-        # State & Attributes
-        self._attr_native_value = None # "online" / "offline"
-        self._attr_extra_state_attributes = {
-            "url": server_url,
-            "response_time_ms": None,
-            "live": None,
-            "viewers": None,
-            "topic": None,
-            "time_online": None
-        }
-
-    async def async_update(self) -> None:
+    async def async_update(self: OwncastParserSensor) -> None:
+        _LOGGER.debug(f"Owncast Tracker {self.name} pulling current data from {self._url}")
         session = async_get_clientsession(self._hass, verify_ssl=self._verify_ssl)
-
+        
         attrs = self._attr_extra_state_attributes
-
         try:
             async with async_timeout.timeout(self._timeout):
                 timeout = aiohttp.ClientTimeout(total=self._timeout)
-                start = self._hass.loop.time()
-                async with session.get(self._server_url, timeout=timeout) as resp:
+                start_time = self._hass.loop.time()
+                async with session.get(self._url, timeout=timeout) as response:
                     data: Any | None = None
-                    content_type = resp.headers.get("Content-Type", "")
+                    content_type = response.headers.get("Content-Type", "")
                     if "application/json" in content_type.lower():
-                        data = await resp.json()
+                        data = await response.json()
                     else:
-                        data = await resp.read()
-                        _LOGGER.debug(F"Owncast status fetch for {self._url} failed: unusual API response: {data}")
-                        self._attr_native_value = "error"
-                elapsed_ms = int((self._hass.loop.time() - start) * 1000)
-                attrs["response_time_ms"] = elapsed_ms
+                        data = await response.read()
+                        _LOGGER.debug(f"Owncast status fetch for {self._url} failed: unusual API response: {data}")
+                        self._attr_native_value = "offline"
+                elapsed_time = int((self._hass.loop.time() - start_time) * 1000)
+                # set reponse time attr
 
                 if isinstance(data, dict):
                     self._attr_available = True
-                    attrs["live"] = data.get("online") or False
-                    attrs["viewers"] = data.get("viewerCount")
-                    attrs["topic"] = "Not Implemented Yet" # FIXME
-                    attrs["time_online"] = "Not Implemented Yet" #FIXME
-
-        except (aiohttp.ClientError, asyncio.TimeoutError) as err:
-            _LOGGER.debug(f"Owncast status check failed for {self._server_url}: {err}")
+                    attrs["viewers"] = data.get("online") or 0
+        except (aiohttp.ClientError, asyncio.TimeoutError) as error:
+            _LOGGER.warning(f"Owncast status check failed for {self._url}: {error}")
             self._attr_native_value = "offline"
             self._attr_available = False
-        except Exception as err:
-            _LOGGER.debug(f"Unexpected error checking {self._server_url}")
+        except Exception as error:
+            _LOGGER.warning(f"Unexpected error checking Owncast Server {self._url}: {error}")
             self._attr_native_value = "offline"
             self._attr_available = False
+        _LOGGER.debug(f"Owncast state updated for {self._url}.")
